2d6ba4456c5ce72de6a2d2920e09665bbe5bf152
Lee chae yeon <yeontachi@gmail.com>
2022-04-18 18:08:18 +0900

    Add files via upload

---
+++ 20220418/test.js
@@ -0,0 +1,26 @@
+console.log(50*8*5*4*6/60);
+console.log(70*0.1);
+console.log(100*0.3);
+console.log(500*5*100*0.0001);
+console.log(1.5*2*5*4);
+
+let tree = {
+  name:['banana', 'streberry', 'apple', 'hapo', 'cat']
+}
+
+console.log(tree.name[3]);
+
+// let doing = {
+//   work:['빨래', '청소', '분리수거', '설거지', '장보기']
+// }
+
+// console.log(doing.work[work.length-1]);
+
+let wark = ['빨래', '청소', '분리수거', '설거지', '장보기']
+
+console.log(wark[wark.length-1]);
+
+// [(  ).length-1] 를 사용하면 제일 마지막인 친구가 나온다고 하는데
+// 위에 처럼  console.log(doing.work[work.length-1]);를 하면 에러가 뜨고
+// console.log(work[work.length-1]);를 사용하면 마지막 배열인 장보기가 나옵니다. 어떻게  console.log(doing.work[work.length-1]); 에서는
+//length-1를 사용할수없는 것인지 궁금합니다. 
