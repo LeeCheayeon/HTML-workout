26bf81e787dfe6b984c6e32f2433bbbd4d70b79c
Lee chae yeon <yeontachi@gmail.com>
2022-06-07 15:22:45 +0900

    hash, innerHTML, a href

---
+++ 20220607/innerHTML/index.js
@@ -0,0 +1,27 @@
+const root =document.getElementById("root");
+
+// const ul = document.createElement("ul");
+// root.appendChild(ul);
+
+// let liElement = []; 
+// for(let i=0; i <10; i++){
+//   // const li = document.createElement('li');
+//   // li.textContent = i;
+//   // ul.appendChild(li);
+//   //-------------------------------------------
+//   // liElement = liElement + `<li> ${i} </li>`;
+//   //-------------------------------------------
+//   liElement.push(`<li> ${i} </li>`);
+// }
+// console.log(liElement.join(""));
+// ul.innerHTML = liElement.join("");
+
+root.innerHTML = `
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+  </ul>
+`;
